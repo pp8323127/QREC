588248a0239e3b519a154d52ffd8d16d6ea92c1a
Chuang Yun Hao <pp8323127@gmail.com>
2016-06-27 19:19:00 +0800

    deploy

--- mobile/db_config.php
+++ mobile/db_config.php
@@ -1,6 +1,12 @@
 <?php
+// $server = 'localhost';
+// $username = 'qrec';
+// $password = 'e508';
+// $database = 'qrec';
+
+//原本的
 $server = 'localhost';
 $username = 'mobile';
 $password = 'e508lab';
-$database = 'mobile';
+$database = 'qrec';
 ?>
