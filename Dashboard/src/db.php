86538795eaf8559eeb57e8ff30853a4bbfce7310
Chuang Yun Hao <pp8323127@gmail.com>
2016-06-27 22:50:04 +0800

    deploy

--- Dashboard/src/db.php
+++ Dashboard/src/db.php
@@ -1,15 +1,15 @@
 <?php
 	error_reporting(0);	//不顯示提醒
-	// $db_server = "localhost";
-	// $db_name = "qrec";
-	// $db_user = "qrec";
-	// $db_passwd = "e508";
+	$db_server = "localhost";
+	$db_name = "qrec";
+	$db_user = "qrec";
+	$db_passwd = "e508";
 
 	// 原本的
-	$db_server = "localhost";
-	$db_name = "mobile";
-	$db_user = "root";
-	$db_passwd = "root_admin";
+	// $db_server = "localhost";
+	// $db_name = "mobile";
+	// $db_user = "root";
+	// $db_passwd = "root_admin";
 	
 	if(!@mysql_connect($db_server, $db_user, $db_passwd))
         die("無法對資料庫連線");
